b09dce0dabb1e4b123c41728531e6dd78153b3cd
hayder <hayderbd4290@gmail.com>
2024-12-30 18:11:03 +0600

    shop card created and pagination done

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,12 +1,13 @@
 const ShopCard = ({ item }) => {
   return (
-    <div className="card bg-gray-50 shadow-xl">
+    <div className="card relative bg-gray-50 shadow-xl">
       <figure>
         <img src={item?.image} alt="Shoes" className="w-full" />
       </figure>
       <div className="card-body">
         <h2 className="text-2xl font-semibold mb-2 text-center">{item?.name}</h2>
         <p className="text-center mb-2">{item?.recipe}</p>
+        <p className="absolute z-10 font-semibold bg-slate-200 rounded-md p-2 text-orange-400 top-4 right-2">${item?.price}</p>
         <div className="card-actions justify-center">
           <button className="btn btn-outline bg-slate-200 border-0 border-b-4 uppercase">
             add to cart
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,20 +1,32 @@
 import { useParams } from "react-router";
 import CoverBanner from "./shared/CoverBanner";
 import bannerImg from '../assets/shop/banner2.jpg'
-import useData from "../hooks/useData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ShopCard from "../components/ShopCard";
+import axios from "axios";
 
 const Shop = () => {
   const { category } = useParams();
-  const [menu] = useData()
   const [catData, setCatData] = useState(category)
+  const [categoryMenu, setCategoryMenu] = useState([])
+  const [totalItems, setTotalItems] = useState(null)
+  const itemPerPage = 6 
+  const pages = Math.ceil(totalItems/itemPerPage);
+  const page = [...Array(pages).keys()]
+  const [currentPage, setCurrentPage]= useState(0)
+ 
 
-  const categoryMenu = menu.filter(fill=> fill.category === catData)
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_ServerUrl}/categoryData/${catData}?page=${currentPage}&limit=${itemPerPage}`)
+    .then(res=>{
+        setCategoryMenu(res.data.data);
+        setTotalItems(res.data.totalItems)
+    })
+  }, [catData, currentPage, itemPerPage])
 
 
   return (
-    <div>
+    <div className="mb-20">
       <CoverBanner
         img={bannerImg}
         title="OUR SHOP"
@@ -22,17 +34,56 @@ const Shop = () => {
       ></CoverBanner>
       <div className="flex justify-center items-center p-8">
         <div className="flex items-center gap-8 md:text-2xl font-semibold">
-            <button onClick={()=> setCatData("salad")} className={`uppercase ${catData==="salad" ? "border-b-4 border-orange-400":""}`}>Salad</button>
-            <button onClick={()=> setCatData("pizza")} className={`uppercase ${catData==="pizza" ? "border-b-4 border-orange-400":""}`}>pizza</button>
-            <button onClick={()=> setCatData("soup")} className={`uppercase ${catData==="soup" ? "border-b-4 border-orange-400":""}`}>soups</button>
-            <button onClick={()=> setCatData("dessert")} className={`uppercase ${catData==="dessert" ? "border-b-4 border-orange-400":""}`}>desserts</button>
+            <button onClick={()=> {
+                setCatData("salad")
+                setCurrentPage(0)
+            }} className={`uppercase ${catData==="salad" ? "border-b-4 border-orange-400":""}`}>Salad</button>
+            <button onClick={()=> {
+                setCatData("pizza")
+                setCurrentPage(0)
+            }} className={`uppercase ${catData==="pizza" ? "border-b-4 border-orange-400":""}`}>pizza</button>
+            <button onClick={()=> {
+                setCatData("soup")
+                setCurrentPage(0)
+            }} className={`uppercase ${catData==="soup" ? "border-b-4 border-orange-400":""}`}>soups</button>
+            <button onClick={()=> {
+                setCatData("dessert")
+                setCurrentPage(0)
+            }} className={`uppercase ${catData==="dessert" ? "border-b-4 border-orange-400":""}`}>desserts</button>
         </div>
       </div>
       {/* card */}
-      <div className="container p-6 mb-20 mx-auto grid gap-14 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+      <div className="container p-6  mx-auto grid gap-14 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         {
             categoryMenu.map(item=> <ShopCard key={item._id} item={item}></ShopCard>)
         }
+      </div>
+      <div className="flex py-6 justify-center items-center">
+        <div className="flex justify-center items-center gap-4">
+            <button
+            onClick={()=> {
+                if(currentPage>0){
+                    setCurrentPage(currentPage - 1)
+                }
+            }}
+             className="p-2 bg-slate-800 text-white px-5 font-semibold rounded-lg">Prev</button>
+            {
+                page.map(pageNo=> (
+                    <button
+                    onClick={()=>setCurrentPage(pageNo)}
+                     key={pageNo} 
+                     className={`w-8 h-8 ${currentPage===pageNo ? "bg-slate-800 text-white" :"bg-slate-200"} flex text-xl items-center justify-center rounded-full p-2 font-bold `}>
+                        {pageNo + 1}</button>
+                ))
+            }
+        <button 
+        onClick={()=>{
+            if(currentPage<page.length -1){
+                setCurrentPage(currentPage + 1)
+            }
+        }}
+        className="p-2 bg-slate-800 text-white px-5 font-semibold rounded-lg">Next</button>
+        </div>
       </div>
     </div>
   );
